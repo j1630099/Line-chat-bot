f10a0f4c968cbe4c96a5fe97ec43a84819d147da
Kane-Ye <85435029+Kane-Ye@users.noreply.github.com>
2021-06-10 20:52:43 +0800

    Update app.py
    增加股市

--- app.py
+++ app.py
@@ -11,6 +11,52 @@ from linebot.models import *
 from crawl_constellation import crawl
 import time
 
+####修改的###
+from Stock_market_crawler import screen ,screen1
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import time
+from pyquery import PyQuery as pq
+from time import sleep
+from sklearn.datasets import make_classification
+import pygsheets
+from database_functions import  get_fortune_index,get_risk,get_budget,record_stock,record_amount
+
+def stock_crawl(FL_ITEM0,FL_VAL_S0, FL_ITEM1,FL_VAL_S1,FL_SHEET,FL_SHEET2,FL_RULE0):
+
+    my_params = {"MARKET_CAT": "自訂篩選", "INDUSTRY_CAT": "我的條件", "FL_ITEM0" : FL_ITEM0 , "FL_VAL_S0" :FL_VAL_S0 ,              "FL_ITEM1": FL_ITEM1 ,"FL_VAL_S1": FL_VAL_S1, "FL_SHEET": FL_SHEET , "FL_SHEET2": FL_SHEET2,"FL_RULE0": FL_RULE0}
+    
+    url = 'https://goodinfo.tw/StockInfo/StockList.asp?'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36' }
+
+    #請求網站
+    list_req = requests.get(url, headers = headers,  params = my_params )
+    browser.get(list_req.url)
+    query_button = browser.find_element_by_css_selector("#MENU10 > tbody > tr:nth-child(2) > td > form > table > tbody > tr:nth-child(19) > td:nth-child(2) > table > tbody > tr > td:nth-child(2) > nobr > input[type=submit]")
+    query_button.click()
+    sleep(0.5)
+    #list_button_ = browser.find_element_by_css_selector("#selSHEET").click()
+    #sleep(0.5)
+    #stoavg_button = browser.find_element_by_css_selector("#selSHEET > option:nth-child(number)".replace("number",str(num))).click()
+    #sleep(15)
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    
+    table = soup.find('table', {'id': 'tblStockList'})
+    df = pd.read_html(table.prettify())
+    df = df[0]
+    df.columns = df.columns.get_level_values(-1)
+    df.columns = df.iloc[0]
+    a = df[df['名稱'].isin(["名稱"])].index
+    df = df.drop(a)
+    
+    return df
+####修改的###
+
+
 app = Flask(__name__)
 
 # Channel Access Token
@@ -304,9 +350,66 @@ def handle_message(event):
         template_message = TemplateSendMessage(
             alt_text='Buttons alt text', template=buttons_template) #alt_text為無法輸出時產生的字樣
         line_bot_api.reply_message(event.reply_token, template_message)
-        
+     
 
 #請接著這裡寫下去
+####修改的###
+    #Risk_Tolerance =  int(get_risk(user_id))
+    #if Risk_Tolerance == 1 :
+    if text == "風險低":
+        Risk_Tolerance == 1 
+        #金融股爬蟲 - 風險承受度低
+        browser = webdriver.Chrome(ChromeDriverManager().install())
+        data_holdings = stock_crawl("近四季–ROA(%)–本季度", 1, "市值 (億元)",0,"股價統計_歷年直接平均","近3年平均均","產業類別||金控業")
+        data_banking = stock_crawl("近四季–ROA(%)–本季度", 1, "市值 (億元)",0,"股價統計_歷年直接平均","近3年平均","產業類別||銀行業")
+        data_Finance = pd.concat([data_holdings, data_banking])
+        Finance_stocks = screen(data_Finance)
+        if len(Finance_stocks) != 0: 
+            #record_stock(user_id, " / ".join(Finance_stocks))
+            message = TextSendMessage(text= " / ".join(Finance_stocks))
+            line_bot_api.push_message(user_id, message)
+
+        else:
+            #record_stock(user_id, "今日無推薦個股")
+            message = TextSendMessage(text= "今日無推薦個股")
+            line_bot_api.push_message(user_id, message)
+
+
+
+    #elif Risk_Tolerance == 2 :
+    elif text == "風險中":
+        Risk_Tolerance == 2
+        #權值股爬蟲-風險承受度中
+        browser = webdriver.Chrome(ChromeDriverManager().install())
+        data = stock_crawl("年度–ROE(%)",15, "市值 (億元)",1700,"股價統計_歷年直接平均","近3年平均","")
+        weighted_stocks = screen(data)
+        if len(weighted_stocks) != 0:
+            #record_stock(user_id, " / ".join(weighted_stocks))
+            message = TextSendMessage(text= " / ".join(weighted_stocks))
+            line_bot_api.push_message(user_id, message)
+        else:
+            #record_stock(user_id, "今日無推薦個股")
+            message = TextSendMessage(text= "今日無推薦個股")
+            line_bot_api.push_message(user_id, message)
+
+
+    #elif Risk_Tolerance == 3 :
+    if text == "風險高":
+        Risk_Tolerance == 3
+        #轉強股爬蟲 - 風險承受度高
+        browser = webdriver.Chrome(ChromeDriverManager().install())
+        data_strong = stock_crawl("當日：紅K棒棒幅(%)", 4, "單月營收年增減率(%)",20,"股價統計_歷年直接平均","近3年平均均","")
+        data_strong = screen1(data_strong)
+        if len(data_strong) != 0:
+            #record_stock(user_id, " / ".join(data_strong))
+            message = TextSendMessage(text= " / ".join(data_strong))
+            line_bot_api.push_message(user_id, message)
+        else:
+            #record_stock(user_id, "今日無推薦個股")
+            message = TextSendMessage(text= "今日無推薦個股")
+            line_bot_api.push_message(user_id, message)
+####修改的###
+
   
 
 import os
