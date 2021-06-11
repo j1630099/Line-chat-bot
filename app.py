813fbe9bc58393e2feca8b317b0067082066f293
Kane-Ye <85435029+Kane-Ye@users.noreply.github.com>
2021-06-11 17:26:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from selenium.webdriver.chrome.options import Options
 import pandas as pd
 from pyquery import PyQuery as pq
 from time import sleep
-from database_functions import  get_fortune_index,get_risk,get_budget,record_stock,record_amount,record_user_info
+from database_functions import  get_fortune_index,get_risk,get_budget,record_stock,record_amount,record_risk,record_user_info
 
 
 
@@ -394,6 +394,7 @@ def handle_message(event):
             line_bot_api.push_message(user_id, message)
             
     elif  text == "風險低":
+        record_risk(user_id, 1)
         data_holdings = stock_crawl("近四季–ROA(%)–本季度", 1, "市值 (億元)",0,"股價統計_歷年直接平均","近3年平均均","產業類別||金控業")
         data_banking = stock_crawl("近四季–ROA(%)–本季度", 1, "市值 (億元)",0,"股價統計_歷年直接平均","近3年平均","產業類別||銀行業")
         data_Finance = pd.concat([data_holdings, data_banking])
@@ -413,6 +414,7 @@ def handle_message(event):
 
     #elif Risk_Tolerance == 2 :
     elif text == "風險中":
+        record_risk(user_id, 2)
        
         #權值股爬蟲-風險承受度中
       
@@ -430,6 +432,7 @@ def handle_message(event):
 
     #elif Risk_Tolerance == 3 :
     elif text == "風險高":
+        record_risk(user_id, 3)
         
         #轉強股爬蟲 - 風險承受度高
 
@@ -443,7 +446,54 @@ def handle_message(event):
             #record_stock(user_id, "今日無推薦個股")
             message = TextSendMessage(text= "今日無推薦個股")
             line_bot_api.push_message(user_id, message)
-       
+    elif type(text) == int:
+        fortune_index = get_fortune_index(user_id)
+        Risk_Tolerance = get_risk(user_id)
+        balance = int(text)
+        if  fortune_index == 1:
+            balance = balance*0.4
+            if Risk_Tolerance == 1:
+                balance = balance*0.82
+            elif Risk_Tolerance == 2:
+                balance = balance*0.91
+            elif Risk_Tolerance == 3:
+                balance = balance*1
+        elif fortune_index == 2:
+            balance = balance*0.55
+            if Risk_Tolerance == 1:
+                balance = balance*0.82
+            elif Risk_Tolerance == 2:
+                balance = balance*0.91
+            elif Risk_Tolerance == 3:
+                balance = balance*1
+        elif  fortune_index == 3:
+            balance = balance*0.75
+            if Risk_Tolerance == 1:
+                balance = balance*0.82
+            elif Risk_Tolerance == 2:
+                balance = balance*0.91
+            elif Risk_Tolerance == 3:
+                balance = balance*1
+        elif  fortune_index == 4:
+            balance = balance*0.9
+            if Risk_Tolerance == 1:
+                balance = balance*0.82
+            elif Risk_Tolerance == 2:
+                balance = balance*0.91
+            elif Risk_Tolerance == 3:
+                balance = balance*1
+        elif  fortune_index == 5:
+            balance = balance*1
+            if Risk_Tolerance == 1:
+                balance = balance*0.82
+            elif Risk_Tolerance == 2:
+                balance = balance*0.91
+            elif Risk_Tolerance == 3:
+                balance = balance*1
+        message = TextSendMessage(text= "今日推薦投入金額：\n"+ round(balance) +"元")
+        line_bot_api.push_message(user_id, message)
+ 
+        
     
     elif text == "結束":
         pass
